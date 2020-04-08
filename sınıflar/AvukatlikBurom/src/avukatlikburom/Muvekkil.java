303b655103a44b1256b2274d6461795df8acf76c
Ali Fuat GEYİK <alifuatgeyik@hotmail.com>
2020-04-08 12:25:51 +0300

    Add files via upload

--- sınıflar/AvukatlikBurom/src/avukatlikburom/Muvekkil.java
+++ sınıflar/AvukatlikBurom/src/avukatlikburom/Muvekkil.java
@@ -1,5 +1,12 @@
 	package avukatlikburom;
 
 public class Muvekkil {
-
+    String soyadi;
+    String iletisimBilgisi;
+    String prestij;
+    String notlar;
+    int tcNo;
+    int vergiNo;    
+    int dosyaNo;
+    double borc;
 }
