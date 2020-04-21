9e0dd0b4832f53f3ae782725d3124ed70cbea577
Ali Fuat GEYİK <alifuatgeyik@hotmail.com>
2020-04-21 17:32:42 +0300

    Add files via upload

---
+++ sınıflar/AvukatlikBurom/src/avukatlikburom/DosyaIslemleri.java
@@ -0,0 +1,17 @@
+package avukatlikburom;
+
+public class DosyaIslemleri {
+
+	public void yaz (String yazi) {
+		
+	}
+	public String oku () {
+		String oku = null;
+		return oku;
+	}
+	public void sirala() {	
+	}
+	public void cikar(String yazi) {
+		
+	}
+}
